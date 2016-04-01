7b92b9432287703643ec20310c63c6ba145a54b3
Heather Rosenberg <h.rosenberg79@gmail.com>
2016-04-01 16:09:48 -0400

    Able to rendor x and o on board

--- Javascript/app.js
+++ Javascript/app.js
@@ -1,10 +1,28 @@
-$(document).ready(function(){
+$(document).ready(function() {
 
 
-    var $gameCells = $('.cell');
+  var $gameCells = $('.cell');
+  var moves = ["", "", "", "", "", "", "", "", ""];
+  //Array(9).join(".").split(".");
+  var count = 0;
 
-  $($gameCells).one('click', function(){
-      console.log(this.id);
-  });
+  $($gameCells).one('click', function() {
+
+    moves[this.id] = this.id;
+    count++;
+    if (count % 2 == 0) {
+      moves[this.id] = "X";
+      $(this).text("X");
+    } else {
+      moves[this.id] = "O";
+      $(this).text("O");
+    }
+    console.log(moves, count);
+  })
+
+
+
+}
+
 
 });
