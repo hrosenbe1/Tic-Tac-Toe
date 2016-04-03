a8c0253d60c5502e86f77b3a52dd0d1ee9aed056
Heather Rosenberg <h.rosenberg79@gmail.com>
2016-04-03 11:10:07 -0400

    Player 2 wins works

--- Javascript/app.js
+++ Javascript/app.js
@@ -35,10 +35,25 @@ $(document).ready(function() {
       winner = "PlayerOne";
       alert("Congratulations, Player 1 wins!");
     }
+  });
+
+  $(".cell").click( function() {
+    $(moves[this.id]).text(PlayerTwo);
+     if ((winner !== PlayerOne) && $("#0").text() === PlayerTwo && $("#1").text() === PlayerTwo && $("#2").text() === PlayerTwo ||
+      $("#3").text() === PlayerTwo && $("#4").text() ===  PlayerTwo && $("#5").text() ===   PlayerTwo ||
+      $("#6").text() === PlayerTwo && $("#7").text() ===  PlayerTwo && $("#8").text() ===   PlayerTwo ||
+      $("#0").text() === PlayerTwo && $("#3").text() === PlayerTwo && $("#6").text() === PlayerTwo ||
+      $("#1").text() === PlayerTwo && $("#4").text() === PlayerTwo && $("#7").text() === PlayerTwo ||
+      $("#2").text() === PlayerTwo && $("#5").text() === PlayerTwo && $("#8").text() === PlayerTwo ||
+      $("#0").text() === PlayerTwo && $("#4").text() === PlayerTwo && $("#8").text() === PlayerTwo ||
+      $("#2").text() === PlayerTwo && $("#4").text() === PlayerTwo && $("#6").text() === PlayerTwo ) {
+      winner = "PlayerOne";
+      alert("Congratulations, Player 2 wins!");
+    }
     if (count === 9 && winner !=="PlayerOne" && winner !== "PlayerTwo") {
       alert("It's a Tie");
     }
-  });
+    });
 
 
 
