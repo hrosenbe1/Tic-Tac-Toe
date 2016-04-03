5d43a68465af6d358b7ea2c5e5a8b5536ae63d66
Heather Rosenberg <h.rosenberg79@gmail.com>
2016-04-03 12:44:05 -0400

    New Game clears board

--- Javascript/app.js
+++ Javascript/app.js
@@ -20,44 +20,45 @@ $(document).ready(function() {
       $(this).text("O");
     }
     console.log(moves, count);
-    })
+  })
 
-  $(".cell").click( function() {
+  $(".cell").click(function() {
     $(moves[this.id]).text(PlayerOne);
-     if ((winner !== PlayerTwo) && $("#0").text() === PlayerOne && $("#1").text() === PlayerOne && $("#2").text() === PlayerOne ||
-      $("#3").text() === PlayerOne && $("#4").text() ===  PlayerOne && $("#5").text() ===   PlayerOne ||
-      $("#6").text() === PlayerOne && $("#7").text() ===  PlayerOne && $("#8").text() ===   PlayerOne ||
+    if ((winner !== PlayerTwo) && $("#0").text() === PlayerOne && $("#1").text() === PlayerOne && $("#2").text() === PlayerOne ||
+      $("#3").text() === PlayerOne && $("#4").text() === PlayerOne && $("#5").text() === PlayerOne ||
+      $("#6").text() === PlayerOne && $("#7").text() === PlayerOne && $("#8").text() === PlayerOne ||
       $("#0").text() === PlayerOne && $("#3").text() === PlayerOne && $("#6").text() === PlayerOne ||
       $("#1").text() === PlayerOne && $("#4").text() === PlayerOne && $("#7").text() === PlayerOne ||
       $("#2").text() === PlayerOne && $("#5").text() === PlayerOne && $("#8").text() === PlayerOne ||
       $("#0").text() === PlayerOne && $("#4").text() === PlayerOne && $("#8").text() === PlayerOne ||
-      $("#2").text() === PlayerOne && $("#4").text() === PlayerOne && $("#6").text() === PlayerOne ) {
+      $("#2").text() === PlayerOne && $("#4").text() === PlayerOne && $("#6").text() === PlayerOne) {
       winner = "PlayerOne";
       alert("Congratulations, Player 1 wins!");
     }
   });
 
-  $(".cell").click( function() {
+  $(".cell").click(function() {
     $(moves[this.id]).text(PlayerTwo);
-     if ((winner !== PlayerOne) && $("#0").text() === PlayerTwo && $("#1").text() === PlayerTwo && $("#2").text() === PlayerTwo ||
-      $("#3").text() === PlayerTwo && $("#4").text() ===  PlayerTwo && $("#5").text() ===   PlayerTwo ||
-      $("#6").text() === PlayerTwo && $("#7").text() ===  PlayerTwo && $("#8").text() ===   PlayerTwo ||
+    if ((winner !== PlayerOne) && $("#0").text() === PlayerTwo && $("#1").text() === PlayerTwo && $("#2").text() === PlayerTwo ||
+      $("#3").text() === PlayerTwo && $("#4").text() === PlayerTwo && $("#5").text() === PlayerTwo ||
+      $("#6").text() === PlayerTwo && $("#7").text() === PlayerTwo && $("#8").text() === PlayerTwo ||
       $("#0").text() === PlayerTwo && $("#3").text() === PlayerTwo && $("#6").text() === PlayerTwo ||
       $("#1").text() === PlayerTwo && $("#4").text() === PlayerTwo && $("#7").text() === PlayerTwo ||
       $("#2").text() === PlayerTwo && $("#5").text() === PlayerTwo && $("#8").text() === PlayerTwo ||
       $("#0").text() === PlayerTwo && $("#4").text() === PlayerTwo && $("#8").text() === PlayerTwo ||
-      $("#2").text() === PlayerTwo && $("#4").text() === PlayerTwo && $("#6").text() === PlayerTwo ) {
+      $("#2").text() === PlayerTwo && $("#4").text() === PlayerTwo && $("#6").text() === PlayerTwo) {
       winner = "PlayerOne";
       alert("Congratulations, Player 2 wins!");
     }
-    if (count === 9 && winner !=="PlayerOne" && winner !== "PlayerTwo") {
+    if (count === 9 && winner !== "PlayerOne" && winner !== "PlayerTwo") {
       alert("It's a Tie");
     }
-    });
+  });
 
-
-
+  $("#winner").click(function() {
+  $("#gameboard div").text('');
 });
 
 
 
+});
