5d0c9942df7e172d6ccbae5f4da9ec97d726e259
Heather Rosenberg <h.rosenberg79@gmail.com>
2016-04-05 09:10:22 -0400

    .on and .off work correctly

--- Javascript/app.js
+++ Javascript/app.js
@@ -8,10 +8,13 @@ $(document).ready(function() {
   var $gameCells = $('.cell');
   var moves = ["#0", "#1", "#2", "#3", "#4", "#5", "#6", "#7", "#8"];
   var count = 0;
+  var score1 = 0;
+  var score2 = 0;
 
 
   //This function adds an X  for player 1 and an O for player 2 for the cell that is clicked
-  $($gameCells).on('click', function() {
+    function startGame(){
+    $($gameCells).on('click', function() {
     moves[this.id] = this.id;
     count++;
     if (count % 2 == 0) {
@@ -23,10 +26,12 @@ $(document).ready(function() {
     }
     console.log(moves, count);
   });
-
-
+      getWinner();
+}
+  startGame();
   //This function gets the winner depending on which cells have X or O and then alerts who the winner is
-  $(".cell").click(function() {
+  function getWinner(){
+  $($gameCells).on('click',function() {
     if ((winner !== PlayerTwo) && $("#0").text() === PlayerOne && $("#1").text() === PlayerOne && $("#2").text() === PlayerOne ||
       $("#3").text() === PlayerOne && $("#4").text() === PlayerOne && $("#5").text() === PlayerOne ||
       $("#6").text() === PlayerOne && $("#7").text() === PlayerOne && $("#8").text() === PlayerOne ||
@@ -36,7 +41,11 @@ $(document).ready(function() {
       $("#0").text() === PlayerOne && $("#4").text() === PlayerOne && $("#8").text() === PlayerOne ||
       $("#2").text() === PlayerOne && $("#4").text() === PlayerOne && $("#6").text() === PlayerOne) {
       winner = "PlayerOne";
-      document.getElementById("score").innerHTML = "<h1>Congratulations, Player 1 wins!</h1";
+      document.getElementById("winner").innerHTML = "<h1>Congratulations, Player 1 wins!</h1";
+      $($gameCells).off('click');
+      score1++;
+
+      console.log(score1);
     } else if ((winner !== PlayerOne) && $("#0").text() === PlayerTwo && $("#1").text() === PlayerTwo && $("#2").text() === PlayerTwo ||
       $("#3").text() === PlayerTwo && $("#4").text() === PlayerTwo && $("#5").text() === PlayerTwo ||
       $("#6").text() === PlayerTwo && $("#7").text() === PlayerTwo && $("#8").text() === PlayerTwo ||
@@ -45,23 +54,27 @@ $(document).ready(function() {
       $("#2").text() === PlayerTwo && $("#5").text() === PlayerTwo && $("#8").text() === PlayerTwo ||
       $("#0").text() === PlayerTwo && $("#4").text() === PlayerTwo && $("#8").text() === PlayerTwo ||
       $("#2").text() === PlayerTwo && $("#4").text() === PlayerTwo && $("#6").text() === PlayerTwo) {
-      winner = "PlayerOne";
-      document.getElementById("score").innerHTML = "<h1>Congratulations, Player 2 wins!</h1";
+      winner = "PlayerTwo";
+      document.getElementById("winner").innerHTML = "<h1>Congratulations, Player 2 wins!</h1>";
+      $($gameCells).off('click');
+      score2++
     } else if (count === 9 && winner !== "PlayerOne" && winner !== "PlayerTwo") {
       winner = "Tie";
-      document.getElementById("score").innerHTML = "<h1>It's a tie! Better luck next time.</h1";
+      document.getElementById("winner").innerHTML = "<h1>It's a tie! Better luck next time.</h1>";
+      $($gameCells).off('click');
     };
+});
 
-
-
+}
 
     //When New Game button is clicked the game is reset
-    $("#button").click(function() {
+    $("#button").on('click', function() {
       count = 0;
       winner = "Default";
-      document.getElementById("score").innerHTML =" ";
-      $(".cell").empty();
+      document.getElementById("winner").innerHTML =" ";
+      $($gameCells).empty();
+      startGame();
+     });
+
     });
 
-  });
-});
