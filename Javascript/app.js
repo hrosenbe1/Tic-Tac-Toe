fdb39a06f2bb3b8b50b0a1007132dbd557b480b8
Heather Rosenberg <h.rosenberg79@gmail.com>
2016-04-06 12:50:11 -0400

    toggle new game button to fix bug

--- Javascript/app.js
+++ Javascript/app.js
@@ -15,6 +15,7 @@ $(document).ready(function() {
 
   //function adds an X  for player 1 and an O for player 2 for the cell that is clicked
   function startGame() {
+    $("#button").hide();
     $($gameCells).one('click', function() {
       moves[this.id] = this.id;
       count++;
@@ -46,6 +47,7 @@ $(document).ready(function() {
         $("#2").text() === PlayerOne && $("#5").text() === PlayerOne && $("#8").text() === PlayerOne ||
         $("#0").text() === PlayerOne && $("#4").text() === PlayerOne && $("#8").text() === PlayerOne ||
         $("#2").text() === PlayerOne && $("#4").text() === PlayerOne && $("#6").text() === PlayerOne) {
+        $("#button").show();
         winner = "PlayerOne";
         score1++;
         audio.play();
@@ -61,6 +63,7 @@ $(document).ready(function() {
         $("#2").text() === PlayerTwo && $("#5").text() === PlayerTwo && $("#8").text() === PlayerTwo ||
         $("#0").text() === PlayerTwo && $("#4").text() === PlayerTwo && $("#8").text() === PlayerTwo ||
         $("#2").text() === PlayerTwo && $("#4").text() === PlayerTwo && $("#6").text() === PlayerTwo) {
+        $("#button").show();
         winner = "PlayerTwo";
         score2++
         audio.play();
@@ -69,6 +72,7 @@ $(document).ready(function() {
         $($gameCells).off('click');
         //condition if there is no winner and the game is a tie
       } else if (count === 9 && winner !== "PlayerOne" && winner !== "PlayerTwo") {
+        $("#button").show();
         winner = "Tie";
         document.getElementById("winner").innerHTML = "<h1>It's a tie! Better luck next time.</h1>";
         $($gameCells).off('click');
@@ -77,13 +81,13 @@ $(document).ready(function() {
   }
 
   //when New Game button is clicked the game is reset
-    $("#button").on('click', function() {
-      count = 0;
-      winner = "Default";
-      document.getElementById("winner").innerHTML = " ";
-      $($gameCells).empty();
-      startGame();
-    });
+  $("#button").on('click', function() {
+    count = 0;
+    winner = "Default";
+    document.getElementById("winner").innerHTML = " ";
+    $($gameCells).empty();
+    startGame();
+  });
 
   //clear score button function
   $("#clearScore").on('click', function() {
